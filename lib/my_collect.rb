8845fa52345960d3f672dc01f720beaf948353a0
Raphael Saphra <dsaphra@Macintosh-c82a1445a595.local>
2019-05-13 08:38:12 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,34 @@
 
+def get_length(collection)
+  collection.length
+end
 
+def my_each(collection)
+  # code here
+  if block_given?
+    i = 0
+    collection_length = get_length(collection)
+    while i < collection_length
+      yield(collection[i])
+      i += 1
+    end
+  end
+  collection
+end
+
+def my_collect(collection)
+  # code here
+  new_collection = []
+  if block_given?
+    i = 0
+    collection_length = get_length(collection)
+    while i < collection_length
+      new_collection_value = yield(collection[i])
+      new_collection.push(new_collection_value)
+      i += 1
+    end
+  new_collection
+  else
+    collection
+  end
+end
